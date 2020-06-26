fedbd818672c91ad36d6879f1e84bb43eaf648ae
MathMark <vadimmartsun@gmail.com>
2020-06-27 01:21:20 +0300

    Add Spring MVC

---
+++ src/main/java/com/pet/mailSender/controllers/IndexController.java
@@ -0,0 +1,19 @@
+/*
+package com.pet.mailSender.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model){
+        System.out.println("fdfgd");
+        return "index";
+    }
+}
+*/
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>Simpe library app</title>
+</head>
+<body>
+<c:out value = "${'<tag> , &'}"/>
+<p>Hello World!</p>
+</body>
+</html>
